727841401ebf059c1c139b9e25408c7c43e75f3f
Melipax <33260323+MeliPax@users.noreply.github.com>
2020-12-04 19:40:44 +0200

    Added models and deplayed the web

--- server.py
+++ server.py
@@ -1,6 +1,77 @@
 import streamlit as st
 import pickle
+import flask
+import pandas as pd
+import pandas as pd
+import numpy as np
+import numpy as np
+import codecs
+import pickle
+import pandas as pd
+from nltk.tokenize import word_tokenize
+import re
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+import nltk
+from sklearn.metrics import classification_report
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.models import load_model
+from tensorflow import keras
 
+
+SEQUENCE_LENGTH = 100
+label2int = {'0': 0, '1': 1}
+int2label = {0: '0', 1: '1'}
+
+# Loading the model
+filename = "customer_churn_model.pkl"
+# model = keras.models.load_model(filename, 'rb')
+model = pickle.load(open(filename, 'rb'))
+# model = load_model("network.h5", 'rb')
+# model = pickle.load(open('network.h5', 'rb'))
+# classifier = pickle.load(model)
+
+# loading the tokenizer
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+def preprocess_text(sen):
+    sentence = remove_tags(sen)
+    sentence = re.sub('[^a-zA-Z]', ' ', str(sentence))
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', str(sentence))
+    sentence = re.sub(r'\s+', ' ', str(sentence))
+
+    return sentence
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+
+def remove_tags(text):
+    return TAG_RE.sub('', str(text))
+
+
+def get_predictions(text):
+    text = preprocess_text(text)
+    sequence = tokenizer.texts_to_sequences([text])
+    # pad the sequence
+    sequence = pad_sequences(sequence, maxlen=SEQUENCE_LENGTH)
+    # get the prediction
+    prediction = model.predict(sequence)[0]
+    # one-hot encoded vector, revert using np.argmax
+    return int2label[np.argmax(prediction)]
+
+# def classify(var):
+#     if()
 
 
 def main():
@@ -10,30 +81,23 @@ def main():
     <h2 style="color:white;text-align:center;">SpamEmail Classification App</h2>
     </div>
     <br><br>
-    <div>
-        <p>Enter email text:</p>
-        <p><textarea  name = "email" rows="4" cols="80"/></p>
-    </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    st.button('Classify')
-   
-    # if st.button('Classify'):
-    #     if option=='Linear Regression':
-    #         st.success(classify(lin_model.predict(inputs)))
-    #     elif option=='Logistic Regression':
-    #         st.success(classify(log_model.predict(inputs)))
-    #     else:
-    #        st.success(classify(svm.predict(inputs)))
+    input = st.text_input("Dependents (0 if no , or 1 if yes)", "")
+    if st.button('Classify'):
+        # if (len(text)
+        # Running prediction
+        st.success(classify(get_predictions(input)))
 
-# def classify(num):
-#     if num < 0.5:
-#         return 'Setosa'
-#     elif num < 1.5:
-#         return 'Versicolor'
-#     else:
-#         return 'Virginica'
+# Printing to the user
+
+
+def classify(val):
+    if val == 0:
+        return 'Spam'
+    else:
+        return 'not spam'
+
+
 if __name__ == '__main__':
     main()
-
-
