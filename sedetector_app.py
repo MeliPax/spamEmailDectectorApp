0486851c2aa9749057292e6bba543b7e6cac23f8
Melipax <33260323+MeliPax@users.noreply.github.com>
2020-12-04 15:24:47 +0200

    initial commit

---
+++ sedetector_app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import codecs
+import pickle
+import pandas as pd
+from nltk.tokenize import word_tokenize
+import re
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+import nltk
+from sklearn.metrics import classification_report
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Embedding
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.models import load_model
+from keras.layers import Embedding, LSTM, Dropout, Dense
---
+++ server.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle
+
+
+
+def main():
+    # st.title("Streamlit Tutorial")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">SpamEmail Classification App</h2>
+    </div>
+    <br><br>
+    <div>
+        <p>Enter email text:</p>
+        <p><textarea  name = "email" rows="4" cols="80"/></p>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    st.button('Classify')
+   
+    # if st.button('Classify'):
+    #     if option=='Linear Regression':
+    #         st.success(classify(lin_model.predict(inputs)))
+    #     elif option=='Logistic Regression':
+    #         st.success(classify(log_model.predict(inputs)))
+    #     else:
+    #        st.success(classify(svm.predict(inputs)))
+
+# def classify(num):
+#     if num < 0.5:
+#         return 'Setosa'
+#     elif num < 1.5:
+#         return 'Versicolor'
+#     else:
+#         return 'Virginica'
+if __name__ == '__main__':
+    main()
+
+
